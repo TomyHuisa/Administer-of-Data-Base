5f6be109bafdc69e799c61ff43f3b08782901fc5
Tomy Huisa <Huisatomy2007@gmail.com>
2024-03-26 16:56:16 -0300

    New

---
+++ EjercicioSQL/Exercises2.sql
@@ -0,0 +1,11 @@
+-- Act1 -- 
+
+SELECT FirstName, LastName FROM employees
+ORDER BY FirstName AND LastName DESC
+
+-- Act2 --
+
+SELECT FirstName, ReportsTo FROM employees WHERE ReportsTo IS NULL
+
+-- Act3 --
+
